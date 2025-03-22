7e9d1b5b235186648ae0ce9fd6d7cad08c763c2e
Joao <Joao@DESKTOP-HSRAFRR>
2025-03-21 21:37:32 -0300

    Adicionar arquivos de projeto.

---
+++ Ensino/Program.cs
@@ -0,0 +1,17 @@
+﻿string a, b;
+
+Console.WriteLine("joao ama livia?");
+a= Console.ReadLine();
+Console.WriteLine("livia ama joao?");
+b = Console.ReadLine();
+if ( (a == "Sim" || a == "sim") & (b=="Sim" || b== "sim") )
+{
+    Console.WriteLine("eh verdaed");
+}
+else if (b == "batata"){
+    Console.WriteLine("batata");
+}
+else
+{
+    Console.WriteLine("eh mentira");
+}
